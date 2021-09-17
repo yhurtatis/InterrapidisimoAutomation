564fe22faa63ec8fd200d6b4826d73e3193b6335
yhurtatis <nataliahurtatis@outlook.com>
2021-09-17 16:48:59 -0500

    CotizaPage class with new atributes

--- src/test/java/pages/CotizaEnvioPage.java
+++ src/test/java/pages/CotizaEnvioPage.java
@@ -3,6 +3,16 @@ package pages;
 import utils.UtilsElement;
 
 public class CotizaEnvioPage extends UtilsElement {
+    private String inputDateXpath="//input[@id='date']";
+    private String inputOrigenXpath="//select[@id=\"Origen\"]";
+    private String inputDestinoXpath="//select[@id=\"Destino\"]";
+    private String inputPesoKilos="//input[@id=\"pesokilos\"]";
+    private String inputPesoReal="//input[@id=\"pesoreal\"]";
+    private String inputValorComercial="//input[@id=\"valorcomercial\"]";
+    private String inputTipoEntrega="//select[@id=\"tipoentrega\"]0";
+    
+
+
 
 
 }
